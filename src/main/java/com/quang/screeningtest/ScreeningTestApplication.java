c90f3e175e33c40217c910ccd084efbda2857542
weng <buiminhquang2002@gmail.com>
2024-08-12 15:13:16 +0700

    Init Project

---
+++ src/main/java/com/quang/screeningtest/ScreeningTestApplication.java
@@ -0,0 +1,13 @@
+package com.quang.screeningtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreeningTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreeningTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quang/screeningtest/ScreeningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quang.screeningtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreeningTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
